3511e63f67d45cb7e88659d3f23712e5b92b2133
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2023-05-01 16:18:36 -0400

    Fix action_description

--- lib/action_description.py
+++ lib/action_description.py
@@ -4,7 +4,7 @@ action_description={
 "desc" : f"""
 Python script that copies a file to two or more locations using shutils.
 
-  Usage: multi-file-copy.py [action] [-opt='options'] <input file>    
+  Usage: multi-file-copy.py [-opt='options'] <input file>    
   Examples: python3 multi-file-copy.py create filename.ext folder_path_copy_destination 
             python3 multi-file-copy.py create -opt 'verbose' filename.ext folder_path_copy_destination
 
